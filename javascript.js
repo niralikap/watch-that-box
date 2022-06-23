a9201a1f15c73e90bfe8b494d3e4691ee404856f
Nilu <nirali.rkapadia@gmail.com>
2022-06-22 19:55:59 -0700

    Adds html and javascript files

---
+++ javascript.js
@@ -0,0 +1,41 @@
+
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+    document.getElementById("box").style.outline = "thick solid blue";
+        });
+
+        
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = "0.2";
+
+        });
+        
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.opacity = "1";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.outline = "thick solid orange";
+        });
+
+document.getElementById("button5").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "25px";
+
+        });
+
+document.getElementById("button6").addEventListener("click", function(){
+
+    document.getElementById("box").style.outline = "thick solid red"; 
+        
+        });
+    
